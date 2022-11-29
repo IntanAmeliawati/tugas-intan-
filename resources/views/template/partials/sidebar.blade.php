88399581378a4e52f1447cb0c4e0481e641505e0
kjovsty <jjovnaggn@gmail.com>
2022-11-29 17:47:23 +0700

    Tugas baru

--- resources/views/template/partials/sidebar.blade.php
+++ resources/views/template/partials/sidebar.blade.php
@@ -9,10 +9,10 @@
     <!-- Sidebar user (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{ asset('adminlte/dist/img/user4-128x128.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Yesica Putri Purba</a>
+          <a href="#" class="d-block">Intan Ameliawati</a>
         </div>
       </div>
 
